55e02cc55e3e8c05fff0980d0b08680c9f8c15c8
vishy00 <vishesh885808@gmail.com>
2026-01-16 22:34:04 +0530

    bug fix

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -10,7 +10,7 @@ export const sellerLogin = async (req, res) => {
             res.cookie("sellerToken", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
             path: "/", // important
             maxAge: 7 * 24 * 60 * 60 * 1000,
             });
@@ -43,7 +43,7 @@ export const sellerLogout = async (req,res) => {
         res.clearCookie('sellerToken', {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict' ,
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax' ,
             path: "/",
         });
         return res.json({ success: true, message: "Logged Out" });
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -25,7 +25,7 @@ export const register = async (req,res) =>{
         res.cookie('token',token, {
             httpOnly: true, //Prevent JavaScript to access cookie
             secure: process.env.NODE_ENV === 'production', //use secure cookie in production
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict' , //CSRF protection
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax' , //CSRF protection
             maxAge: 7 * 24 * 60 * 60 * 1000, //Cookie expiration time
         })
 
@@ -63,7 +63,7 @@ export const login = async (req,res) => {
         res.cookie('token',token, {
             httpOnly: true, 
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict' ,
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             maxAge: 7 * 24 * 60 * 60 * 1000, 
         })
 
@@ -93,7 +93,7 @@ export const logout = async (req,res) => {
         res.clearCookie('token', {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict' ,
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         });
         return res.json({ success: true, message: "Logged Out" });
     } catch (error) {

9e4d04556ad6dfe564c048b3a1c3e1a6f17d04bc
vishy00 <vishesh885808@gmail.com>
2026-01-16 21:17:57 +0530

    fix bug

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ await connectDB();
 await connectCloudinary();
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://farmly-frontend.vercel.app']  
 
 //Middleware configuration
 app.use(cors({

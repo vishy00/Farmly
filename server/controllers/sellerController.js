75f151a853d097a708e01087df89c695ba2b9fcf
vishy00 <vishesh885808@gmail.com>
2026-01-16 23:14:01 +0530

    last change for today

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -27,13 +27,15 @@ export const sellerLogin = async (req, res) => {
     }
 }
 
-// Seller isAuth : /api/seller/is-auth
-export const isSellerAuth = async (req,res) => {
+export const isSellerAuth = async (req, res) => {
     try {
-        return res.json({success: true })
+        const { sellerToken } = req.cookies;
+        if (!sellerToken) {
+            return res.json({ success: false, message: "No Token" });
+        }
+        return res.json({ success: true });
     } catch (error) {
-        console.log(error.message);
-        res.json( { success: false, message: error.message } );
+        return res.json({ success: false, message: error.message });
     }
 }
 
